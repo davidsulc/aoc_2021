bf643e7c90048079d54c3e5085396ef129195b8e
David Sulc <d.sulc@bluecode.com>
2021-12-02 09:49:32 +0100

    refactor

--- 02/sol/lib/sol/position.ex
+++ 02/sol/lib/sol/position.ex
@@ -12,8 +12,7 @@ defmodule Sol.Position do
   def apply(%__MODULE__{} = pos, %Move{direction: direction} = move, use_aim? \\ false) do
     case direction do
       :forward -> advance(pos, move, use_aim?)
-      :down -> dive(pos, move, use_aim?)
-      :up -> surface(pos, move, use_aim?)
+      up_or_down when up_or_down in ~w(up down)a -> apply_vertical_change(pos, move, use_aim?)
     end
   end
 
@@ -26,17 +25,16 @@ defmodule Sol.Position do
     end
   end
 
-  defp dive(%__MODULE__{} = pos, %Move{count: count}, use_aim?) do
-    case use_aim? do
-      false -> %{pos | depth: pos.depth + count}
-      true -> %{pos | aim: pos.aim + count}
-    end
+  defp apply_vertical_change(pos, move, use_aim?) do
+    attr_to_change =
+      case use_aim? do
+        true -> :aim
+        false -> :depth
+      end
+
+    Map.update!(pos, attr_to_change, position_updater(move))
   end
 
-  defp surface(%__MODULE__{} = pos, %Move{count: count}, use_aim?) do
-    case use_aim? do
-      false -> %{pos | depth: pos.depth - count}
-      true -> %{pos | aim: pos.aim - count}
-    end
-  end
+  defp position_updater(%Move{direction: :down, count: c}), do: &(&1 + c)
+  defp position_updater(%Move{direction: :up, count: c}), do: &(&1 - c)
 end
