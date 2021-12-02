d4d83e91de765625312c16f931df5e8463a0e21a
David Sulc <d.sulc@bluecode.com>
2021-12-02 08:49:42 +0100

    initial commit Day 2

---
+++ 02/sol/lib/sol.ex
@@ -0,0 +1,18 @@
+defmodule Sol do
+  @moduledoc """
+  Documentation for `Sol`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sol.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
