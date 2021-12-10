a2d6361f8c869af36254704fa7233f48e04020ea
David Sulc <d.sulc@bluecode.com>
2021-12-10 10:00:16 +0100

    Day 10, part 2

--- 10/sol/lib/sol.ex
+++ 10/sol/lib/sol.ex
@@ -6,28 +6,61 @@ defmodule Sol do
     ~w/< >/
   ]
 
-  @matching @pairs |> Enum.map(fn [open, close] -> {close, open} end) |> Enum.into(%{})
+  @by_opening @pairs |> Enum.map(&List.to_tuple/1) |> Enum.into(%{})
+
+  @by_closing @pairs |> Enum.map(fn [open, close] -> {close, open} end) |> Enum.into(%{})
 
   @opening Enum.map(@pairs, &hd/1)
 
-  def part_1() do
+  defp input() do
     ~w(priv input.txt)
     |> Path.join()
     |> File.read!()
-    |> solve_1()
+  end
+
+  def part_1() do
+    solve_1(input())
+  end
+
+  def part_2() do
+    solve_2(input())
   end
 
   def solve_1(input) do
     input
-    |> String.split("\n", trim: true)
-    |> Enum.map(&parse_line/1)
+    |> parse_input()
     |> Enum.map(&evaluate/1)
     |> Enum.flat_map(fn
-      :ok -> []
+      {:ok, _} -> []
       {:error, tok} -> [tok]
     end)
-    |> Enum.map(&score/1)
+    |> Enum.map(&score_1/1)
     |> Enum.sum()
+  end
+
+  def solve_2(input) do
+    sorted_scores =
+      input
+      |> parse_input()
+      |> Enum.filter(fn l ->
+        case evaluate(l) do
+          {:ok, _} -> true
+          _ -> false
+        end
+      end)
+      |> Enum.map(&complete/1)
+      |> Enum.map(&score_2/1)
+      |> Enum.sort()
+
+    index = sorted_scores |> length() |> div(2)
+
+    Enum.at(sorted_scores, index)
+  end
+
+  defp parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
   end
 
   defp parse_line(line) do
@@ -36,7 +69,7 @@ defmodule Sol do
 
   defp evaluate([h | t]), do: evaluate(t, [h])
 
-  defp evaluate([], _), do: :ok
+  defp evaluate([], acc), do: {:ok, acc}
 
   defp evaluate([open | rest], acc) when open in @opening do
     evaluate(rest, [open | acc])
@@ -50,10 +83,16 @@ defmodule Sol do
   end
 
   defp matching_closing?(closing, opening) do
-    case Map.get(@matching, closing) do
+    case Map.get(@by_closing, closing) do
       ^opening -> true
       _ -> false
     end
+  end
+
+  def complete(line) do
+    {:ok, unmatched} = evaluate(line)
+
+    Enum.map(unmatched, &Map.get(@by_opening, &1))
   end
 
   [
@@ -63,6 +102,20 @@ defmodule Sol do
     {">", 25137}
   ]
   |> Enum.each(fn {tok, score} ->
-    defp score(unquote(tok)), do: unquote(score)
+    defp score_1(unquote(tok)), do: unquote(score)
+  end)
+
+  defp score_2(line), do: score_2(line, 0)
+
+  defp score_2([], acc), do: acc
+
+  [
+    {")", 1},
+    {"]", 2},
+    {"}", 3},
+    {">", 4}
+  ]
+  |> Enum.each(fn {tok, score} ->
+    defp score_2([unquote(tok) | rest], acc), do: score_2(rest, acc * 5 + unquote(score))
   end)
 end
--- 10/sol/test/sol_test.exs
+++ 10/sol/test/sol_test.exs
@@ -18,4 +18,18 @@ defmodule SolTest do
   test "Part 1" do
     assert Sol.solve_1(@input) == 26397
   end
+
+  test "Part 2" do
+    assert Sol.solve_2(@input) == 288957
+  end
+
+  test "complete/1" do
+    result =
+      "[({(<(())[]>[[{[]{<()<>>"
+      |> String.split("", trim: true)
+      |> Sol.complete()
+      |> Enum.join()
+
+    assert result == "}}]])})]"
+  end
 end
