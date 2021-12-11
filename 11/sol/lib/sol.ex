1320412cd8a2d139fd34a61a5770c79759ed1835
David Sulc <davidsulc@gmail.com>
2021-12-11 11:37:17 +0100

    Day 11, part 1

---
+++ 11/sol/lib/sol.ex
@@ -0,0 +1,167 @@
+defmodule Sol do
+  defmodule Grid do
+    defstruct [:points]
+
+    def from_string(string) when is_binary(string) do
+      points =
+        string
+        |> String.split("\n", trim: true)
+        |> Enum.map(fn line ->
+          line
+          |> String.split("", trim: true)
+          |> Enum.map(&String.to_integer/1)
+        end)
+
+      %__MODULE__{points: points}
+    end
+
+    def map(%__MODULE__{} = grid, mapper) when is_function(mapper, 1) do
+      map(grid, fn point, _coord -> mapper.(point) end)
+    end
+
+    def map(%__MODULE__{points: points} = grid, mapper) when is_function(mapper, 2) do
+      updated_points =
+        points
+        |> Enum.with_index()
+        |> Enum.map(fn {line, row_index} ->
+          line
+          |> Enum.with_index()
+          |> Enum.map(fn {point, col_index} ->
+            mapper.(point, {row_index, col_index})
+          end)
+        end)
+
+      %{grid | points: updated_points}
+    end
+
+    def filter(%__MODULE__{points: grid}, filter) when is_function(filter, 2) do
+      grid
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {line, row_index} ->
+        line
+        |> Enum.with_index()
+        |> Enum.reduce([], fn {point, col_index}, acc ->
+          coord = {row_index, col_index}
+
+          case filter.(point, coord) do
+            true -> [{point, coord} | acc]
+            false -> acc
+          end
+        end)
+      end)
+    end
+
+    def to_string(%__MODULE__{} = grid), do: String.Chars.to_string(grid)
+  end
+
+  defimpl String.Chars, for: Grid do
+    def to_string(%{points: points}) do
+      points
+      |> Enum.map(fn line -> Enum.map(line, &Integer.to_string/1) end)
+      |> Enum.intersperse("\n")
+      |> IO.iodata_to_binary()
+    end
+  end
+
+  @input """
+  4585612331
+  5863566433
+  6714418611
+  1746467322
+  6161775644
+  6581631662
+  1247161817
+  8312615113
+  6751466142
+  1161847732
+  """
+
+  def part_1() do
+    {_, count} =
+      @input
+      |> Grid.from_string()
+      |> step(100)
+
+    count
+  end
+
+  def step(%Grid{} = grid, steps) do
+    Enum.reduce(1..steps, {grid, 0}, fn _, {grid, flash_count} ->
+      {grid, flash_coords} = step(grid)
+      {grid, flash_count + MapSet.size(flash_coords)}
+    end)
+  end
+
+  def step(%Grid{} = grid) do
+    step(:cont, Grid.map(grid, & &1 + 1), MapSet.new())
+  end
+
+  defp step(:halt, grid, acc_flashes), do: {grid, acc_flashes}
+
+  defp step(:cont, grid, acc_flashes) do
+    {grid, new_flashes} = trigger_flashes(grid)
+    all_flashes = MapSet.union(acc_flashes, new_flashes)
+
+    {continue?, grid} =
+      case MapSet.size(new_flashes) do
+        0 -> {:halt, grid}
+        _ ->
+          # we need to increment the cells in the blast radius
+          # of cells having flashed, unless the cell itself has flashed
+          grid = Grid.map(grid, fn level, coord ->
+            case MapSet.member?(all_flashes, coord) do
+              true -> level
+              false -> level + count_neighbors(new_flashes, coord)
+            end
+          end)
+
+          {:cont, grid}
+      end
+
+    step(continue?, grid, all_flashes)
+  end
+
+  defp count_neighbors(coords, {_x, _y} = cell) do
+    coords
+    |> MapSet.intersection(neighbors(cell))
+    |> MapSet.size()
+  end
+
+  defp neighbors({x, y}) do
+    neighbors =
+      for coord_x <- (x - 1)..(x + 1),
+          coord_x >= 0,
+          coord_x <= 10,
+          coord_y <- (y - 1)..(y + 1),
+          coord_y >= 0,
+          coord_y <= 10 do
+        {coord_x, coord_y}
+      end
+
+    MapSet.new(neighbors)
+  end
+
+  # returns {grid, cells} where
+  # * grid has been updated to set the levels of cells having
+  #     flashed back down to 0
+  # * cells is a map set of coordinates for the cells that flashed
+  defp trigger_flashes(%Grid{} = grid) do
+    flash_triggered? = fn level, _coord -> level > 9 end
+
+    flash_coords =
+      grid
+      |> Grid.filter(flash_triggered?)
+      |> Enum.map(&elem(&1, 1))
+      |> MapSet.new()
+
+    grid =
+      Grid.map(grid, fn level, coord ->
+        case flash_triggered?.(level, coord) do
+          true -> 0
+          false -> level
+        end
+      end)
+
+    {grid, flash_coords}
+  end
+end
---
+++ 11/sol/test/sol_test.exs
@@ -0,0 +1,112 @@
+defmodule SolTest do
+  use ExUnit.Case
+  doctest Sol
+
+  @small_input """
+  11111
+  19991
+  19191
+  19991
+  11111
+  """
+
+  alias Sol.Grid
+
+  defp assert_grid(expected, actual) do
+    assert String.trim_trailing(expected, "\n") == Grid.to_string(actual)
+  end
+
+  test "step/1" do
+    {grid, _} =
+      @small_input
+      |> Grid.from_string()
+      |> Sol.step()
+
+    assert_grid("""
+    34543
+    40004
+    50005
+    40004
+    34543
+    """, grid)
+
+    {grid, _} = Sol.step(grid)
+
+    assert_grid("""
+    45654
+    51115
+    61116
+    51115
+    45654
+    """, grid)
+  end
+
+  test "step/2" do
+    grid = Grid.from_string(@small_input)
+
+    {grid, flash_count} = Sol.step(grid, 2)
+
+    assert_grid("""
+    45654
+    51115
+    61116
+    51115
+    45654
+    """, grid)
+
+    assert flash_count == 9
+
+    grid_string = """
+      5483143223
+      2745854711
+      5264556173
+      6141336146
+      6357385478
+      4167524645
+      2176841721
+      6882881134
+      4846848554
+      5283751526
+      """
+
+    {grid, flash_count} =
+      grid_string
+      |> Grid.from_string()
+      |> Sol.step(10)
+
+      assert_grid("""
+        0481112976
+        0031112009
+        0041112504
+        0081111406
+        0099111306
+        0093511233
+        0442361130
+        5532252350
+        0532250600
+        0032240000
+        """, grid)
+
+      assert flash_count == 204
+
+    {grid, flash_count} =
+      grid_string
+      |> Grid.from_string()
+      |> Sol.step(100)
+
+      assert_grid("""
+        0397666866
+        0749766918
+        0053976933
+        0004297822
+        0004229892
+        0053222877
+        0532222966
+        9322228966
+        7922286866
+        6789998766
+        """, grid)
+
+      assert flash_count == 1656
+  end
+end
