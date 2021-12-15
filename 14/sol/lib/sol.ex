caaea7bd76dcfbf748b9a0534012ee3858dd19ba
David Sulc <d.sulc@bluecode.com>
2021-12-15 16:09:23 +0100

    Day 14, part 2

--- 14/sol/lib/sol.ex
+++ 14/sol/lib/sol.ex
@@ -38,10 +38,14 @@ defmodule Sol do
   defp transform_once(rules, polymer) when is_list(polymer) do
     polymer_parts =
       polymer
-      |> Enum.chunk_every(2, 1, :discard)
-      |> Enum.map(&apply_to(rules, &1))
+      |> Stream.chunk_every(2, 1, :discard)
+      |> Stream.map(&apply_to(rules, &1))
+      |> Enum.into([])
 
-    [hd(polymer_parts) |> hd() | Enum.flat_map(polymer_parts, fn [_x, y, z] -> [y, z] end)]
+    [
+      hd(polymer_parts) |> hd()
+      | Enum.flat_map(polymer_parts, fn [_x, y, z] -> [y, z] end)
+    ]
   end
 
   defp transform(polymer, opts) do
@@ -63,10 +67,68 @@ defmodule Sol do
       path
       |> parse_polymer()
       |> transform(rules: rules, passes: 9)
-      |> Enum.group_by(& &1)
-      |> Enum.map(fn {k, v} -> {k, length(v)} end)
+      |> Enum.frequencies()
       |> Enum.min_max_by(fn {_k, v} -> v end)
 
     max - min
+  end
+
+  def part_1_b(path \\ @path), do: solve_for(path, 10)
+
+  def part_2(path \\ @path), do: solve_for(path, 40)
+
+  defp solve_for(path, passes) do
+    rules = parse_rules(path)
+    polymer = parse_polymer(path)
+
+    pairs =
+      polymer
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.frequencies()
+      |> Enum.into(%{})
+
+    letter_counts =
+      polymer
+      |> Enum.frequencies()
+      |> Enum.into(%{})
+
+    state = %{
+      rules: rules,
+      pairs: pairs,
+      letter_counts: letter_counts
+    }
+
+    solve(state, passes)
+  end
+
+  defp solve(%{letter_counts: counts}, 0) do
+    {{_, min}, {_, max}} = Enum.min_max_by(counts, fn {_, c} -> c end)
+    max - min
+  end
+
+  defp solve(%{} = state, iterations) do
+    state.pairs
+    |> Enum.reduce(state, fn {pair, count}, acc ->
+      case Map.get(state.rules, pair) do
+        nil ->
+          acc
+
+        i ->
+          [left, right] = pair
+
+          updated_pairs =
+            acc.pairs
+            |> Map.update!(pair, &(&1 - count))
+            |> Map.update([left, i], count, &(&1 + count))
+            |> Map.update([i, right], count, &(&1 + count))
+
+          %{
+            acc
+            | letter_counts: Map.update(acc.letter_counts, i, count, &(&1 + count)),
+              pairs: updated_pairs
+          }
+      end
+    end)
+    |> solve(iterations - 1)
   end
 end
--- 14/sol/test/sol_test.exs
+++ 14/sol/test/sol_test.exs
@@ -7,4 +7,12 @@ defmodule SolTest do
   test "Part 1" do
     assert Sol.part_1(@path) == 1588
   end
+
+  test "Part 1 b" do
+    assert Sol.part_1_b(@path) == 1588
+  end
+
+  test "Part 2" do
+    assert Sol.part_2(@path) == 2_188_189_693_529
+  end
 end
